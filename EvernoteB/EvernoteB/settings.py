dfce0c0452458fb597ab382ee2e8d3fd87c3ffa9
Saulo Gomes <saulo.gomes@reitoria.ifpe.edu.br>
2023-08-14 10:31:01 -0300

    Uso do Crispy Forms

--- EvernoteB/EvernoteB/settings.py
+++ EvernoteB/EvernoteB/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'paginas.apps.PaginasConfig',
     'cadastros.apps.CadastrosConfig',
 ]
@@ -127,3 +129,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy Forms
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
